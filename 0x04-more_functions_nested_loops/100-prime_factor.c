9bcb16993c6021f7e4e3845665e894935996cd8e
kareema <kareemayasminimam@gmail.com>
2023-02-24 12:42:05 -0800

    i

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -16,19 +16,19 @@ void print_triangle(int size)
 			for (y = 1; y <= size; y++)
 			{
 				if (y <= size - x)
-				{
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar('#');
-				}
-			}
-			_putchar('\n');
-		}
-	}
-	else
-	{
-		_putchar('\n');
-	}
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
